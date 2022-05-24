7f98108051b4d26ece6392783e13b2dd73396591
@Diego-Angeles-Ake <angeles.diego97@gmail.com>
2022-05-24 17:51:57 -0500

    Updated db config

--- database/database.config.js
+++ database/database.config.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config({ path: './.env' });
 
 const sequelize = new Sequelize({
-  dialect: process.env.DATABASE_DIALECT,
+  dialect: 'postgres',
   storage: process.env.DATABASE_HOST,
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
